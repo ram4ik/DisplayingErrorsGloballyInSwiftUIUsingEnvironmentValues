50036c4db512e4fd725a1b2dffc5d225b9bb7f2a
Ramill <ramill_ibragimov@icloud.com>
2023-09-02 19:51:19 +0300

    Initial Commit

---
+++ DisplayingErrorsGloballyInSwiftUIUsingEnvironmentValues/DisplayingErrorsGloballyInSwiftUIUsingEnvironmentValuesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayingErrorsGloballyInSwiftUIUsingEnvironmentValuesApp.swift
+//  DisplayingErrorsGloballyInSwiftUIUsingEnvironmentValues
+//
+//  Created by Ramill Ibragimov on 02.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DisplayingErrorsGloballyInSwiftUIUsingEnvironmentValuesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
